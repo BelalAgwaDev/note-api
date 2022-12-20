9db0852beae7e3ae790c0e4f06d35c9ceb283fc4
belal agwa <belalagwa0@gmail.com>
2022-12-21 01:39:37 +0200

    add storage in note

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,10 +1,44 @@
+var noteGeneratorId=require("../util/generator")
+var noteModel=require("../model/note.model")
+var memoryStorage=require("../util/memory.storage")
+
+
+
+
 
 exports.getAllNotes=function(req,res){
-    res.send("get all notes..........")
+    var seqId=noteGeneratorId.generate()
+    memoryStorage.myStorage.setItem(seqId, '1st item');
+
+    var seqId2=noteGeneratorId.generate()
+    memoryStorage.myStorage.setItem(seqId2, '2nd item');
+
+    var keys =memoryStorage.getKays(memoryStorage.myStorage)
+    var values =memoryStorage.getValues(memoryStorage.myStorage)
+    console.log(JSON.stringify(keys))
+
+
+    var note=noteModel.note
+    var noteObject= new note(seqId,"ccc","dddd","mmmmmm",new Date())
+    res.send(JSON.stringify(noteObject))
 }
 
 
 exports.saveNotes=function(req,res){
+    var seqId=noteGeneratorId.generate()
+    var title=req.body.title
+    var createdby="admin"
+    var createdOn=new Date()
+    var content=req.body.context
+    if(!title || !content ){
+        res.status(422).send({ error: 'Title and content not be empty' })
+    }
+
+
+    var note=noteModel.note
+    var noteObject= new note(seqId,title,content,createdby,createdOn)
+
+
     res.send("save user..........")
 }
 
---
+++ model/note.model.js
@@ -0,0 +1,9 @@
+exports.note=class note{
+constructor(noteId,title,context,createdby,createdOn){
+    this.noteId=noteId
+    this.title=title
+    this.context=context
+    this.createdby=createdby
+    this.createdOn=createdOn
+}
+}
