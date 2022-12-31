b338bfdff8dc681a0f261af968e2232a238e32ae
belal agwa <belalagwa0@gmail.com>
2022-12-31 18:09:00 +0200

    mongo data base connection

--- database/connection.js
+++ database/connection.js
@@ -2,4 +2,25 @@ const mongoose = require('mongoose');
 
 
 
-database_url=""
+database_url="mongodb+srv://belalagwa:2468100@cluster0.selumet.mongodb.net/tasks?retryWrites=true&w=majority"
+
+
+
+const connectDb=function(url){
+    mongoose.connect(database_url,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    
+    }).then((result) => {
+        console.log("database is connected")
+    }).catch((err) => {
+        console.log(err)
+    });
+    
+}
+
+
+
+
+
+module.exports=connectDb
--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 
 var express = require('express')
-var bodyParser = require('body-parser')
-var cors=require("cors")
 const noteRouter = require('./route/noteRoute')
 const taskRoute = require('./route/taskRoute');
+const dotenv = require('dotenv').config();
+const connectDb=require('./database/connection');
 const app=express()
 const port=3000
 
 
-//  app.use(cors())
-
-
-// // // parse application/x-www-form-urlencoded
-//  app.use(bodyParser.urlencoded({ extended: false }))
-
-// // parse application/json
  app.use(express.json())
 
 
 
 
-// use note route
 app.use("/api/v1/note",noteRouter)
 
 app.use("/api/v1/task",taskRoute)
 
-app.listen(port,()=>{
-   
-    console.log(`server start .............`);
-})
+
+
+
+
+
+
+
+const start=async function(){
+    try {
+        await connectDb(process.env.DATABASE_URL)
+        app.listen(port,()=>{
+            console.log(`server start .............`);
+        })
+
+       
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+start()
 
